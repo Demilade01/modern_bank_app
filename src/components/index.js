909ef7e4e3ab5004a7e1d6fdf4383d7df150db54
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-02-19 13:46:29 -0800

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Stats,
+  Footer,
+  GetStarted,
+  Testimonials,
+  Hero,
+};
